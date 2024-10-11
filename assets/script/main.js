00ec75c88fcb978045707e84fef9720d0e2ea6b9
nom519 <wasgm519nh@gmail.com>
2024-10-11 16:06:12 +0900

    10/11lp2作成

--- assets/script/main.js
+++ assets/script/main.js
@@ -2,12 +2,24 @@ $(document).ready(function(){
 	$('.carousel').slick({
 		autoplay: false, // 自動再生
 		autoplaySpeed: 5000, // 自動再生の速さ、単位はミリ秒
+        slidesToShow:1,
+        slidesToScroll:1,
 		arrows: true, // 左右のナビゲーションを無効にする
 		dots: true, // スライド下にドットを表示する
 		fade: true, // スライドの切り替えをフェードにする
         prevArrow: '<button type="button" class="slick-prev"><img src="/assets/image/arrow-left-circle-fill.svg" alt="" class="prev-arrow"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="/assets/image/arrow-right-circle-fill.svg" alt="" class="next-arrow"></button>',
+
+        responsive:[{
+            breakpoint:768,
+            settings:{
+                slidesToShow:1,
+                slidesToScroll:1,
+            },
+        },
+    ]
 	});
+
 });
 $(function(){
     $('.js-accordion-title').click(function(){
@@ -17,17 +29,17 @@ $(function(){
 });
 
 $(function(){
-    var pagetop = $('#page_top');
+    var pagetop = $('#page-top');
     pagetop.hide();
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
+    $(window).scroll(function(){
+        if ($(this).scrollTop() >100) {
             pagetop.fadeIn();
         } else {
             pagetop.fadeOut();
         }
     });
-    pagetop.click(function () {
-        $('body, html').animate({ scrollTop: 0 }, 500);
+    pagetop.click(function(){
+        $('body, html').animate({ scrollTop:0}, 600);
         return false;
     });
 });
