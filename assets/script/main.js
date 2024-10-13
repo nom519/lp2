f29bd874c30040e4e2cbca7b1e0da9c9fd338acc
nom519 <wasgm519nh@gmail.com>
2024-10-13 22:14:30 +0900

    10/13lp2課題提出

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,16 +1,15 @@
 $(document).ready(function(){
 	$('.carousel').slick({
-		autoplay: false, // 自動再生
-		autoplaySpeed: 5000, // 自動再生の速さ、単位はミリ秒
+		autoplay: false,
+		autoplaySpeed: 5000,
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 3,
-		arrows: true, // 左右のナビゲーションを無効にする
-		dots: true, // スライド下にドットを表示する
-		fade: false, // スライドの切り替えをフェードにする
+		arrows: true,
+		dots: true,
+		fade: false,
         prevArrow: '<button type="button" class="slick-prev"><img src="/assets/image/arrow-left-circle-fill.svg" alt="" class="prev-arrow"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="/assets/image/arrow-right-circle-fill.svg" alt="" class="next-arrow"></button>',
-
         responsive:[{
             breakpoint:768,
             settings:{
