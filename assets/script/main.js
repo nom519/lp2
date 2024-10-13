5c4a8999a53c5745da716be0c9fa7da28699407e
nom519 <wasgm519nh@gmail.com>
2024-10-13 15:28:30 +0900

    10/13lp2作成

--- assets/script/main.js
+++ assets/script/main.js
@@ -2,19 +2,20 @@ $(document).ready(function(){
 	$('.carousel').slick({
 		autoplay: false, // 自動再生
 		autoplaySpeed: 5000, // 自動再生の速さ、単位はミリ秒
-        slidesToShow:3,
-        slidesToScroll:1,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
 		arrows: true, // 左右のナビゲーションを無効にする
 		dots: true, // スライド下にドットを表示する
-		fade: true, // スライドの切り替えをフェードにする
+		fade: false, // スライドの切り替えをフェードにする
         prevArrow: '<button type="button" class="slick-prev"><img src="/assets/image/arrow-left-circle-fill.svg" alt="" class="prev-arrow"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="/assets/image/arrow-right-circle-fill.svg" alt="" class="next-arrow"></button>',
 
         responsive:[{
             breakpoint:768,
             settings:{
-                slidesToShow:1,
-                slidesToScroll:1,
+                slidesToShow: 1,
+                slidesToScroll: 1,
             },
         },
     ]
